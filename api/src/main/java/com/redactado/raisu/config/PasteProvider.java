29a57fec1b3243159bfa8753665bfa00e914de99
Reddishye <noreply@redactado.es>
2026-02-23 15:06:22 +0100

    feat PasteProvider: add shortId field for provider identification

--- api/src/main/java/com/redactado/raisu/config/PasteProvider.java
+++ api/src/main/java/com/redactado/raisu/config/PasteProvider.java
@@ -1,13 +1,19 @@
 package com.redactado.raisu.config;
 
 public enum PasteProvider {
-    PASTES_DEV("https://pastes.dev"),
-    HASTEBIN("https://hastebin.com");
+    PASTES_DEV("P", "https://pastes.dev"),
+    HASTEBIN("H", "https://hastebin.com");
 
+    private final String shortId;
     private final String defaultUrl;
 
-    PasteProvider(String defaultUrl) {
+    PasteProvider(String shortId, String defaultUrl) {
+        this.shortId = shortId;
         this.defaultUrl = defaultUrl;
+    }
+
+    public String shortId() {
+        return shortId;
     }
 
     public String getDefaultUrl() {
