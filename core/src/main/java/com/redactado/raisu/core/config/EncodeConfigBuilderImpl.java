0a53ed1a2957864f77b36bd381938958cd641187
Reddishye <noreply@redactado.es>
2026-02-23 15:42:12 +0100

    refactor EncodeConfig: remove encryption and password fields from interface and builder

--- core/src/main/java/com/redactado/raisu/core/config/EncodeConfigBuilderImpl.java
+++ core/src/main/java/com/redactado/raisu/core/config/EncodeConfigBuilderImpl.java
@@ -3,15 +3,10 @@ package com.redactado.raisu.core.config;
 import com.redactado.raisu.config.EncodeConfig;
 import com.redactado.raisu.config.PasteProvider;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public final class EncodeConfigBuilderImpl implements EncodeConfig.Builder {
 
     private PasteProvider provider = PasteProvider.HASTEBIN;
-    private boolean encrypt = false;
-
-    @Nullable
-    private String password = null;
 
     @Override
     @NotNull
@@ -22,25 +17,7 @@ public final class EncodeConfigBuilderImpl implements EncodeConfig.Builder {
 
     @Override
     @NotNull
-    public EncodeConfig.Builder encrypt(boolean encrypt) {
-        this.encrypt = encrypt;
-        return this;
-    }
-
-    @Override
-    @NotNull
-    public EncodeConfig.Builder password(@NotNull String password) {
-        this.password = password;
-        this.encrypt = true;
-        return this;
-    }
-
-    @Override
-    @NotNull
     public EncodeConfig build() {
-        if (encrypt && password == null) {
-            throw new IllegalStateException("Password required when encryption is enabled");
-        }
-        return new EncodeConfigImpl(provider, encrypt, password);
+        return new EncodeConfigImpl(provider);
     }
 }
