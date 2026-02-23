1bb48eeb0f46698662e80b6ef0cb357881d21fa3
Reddishye <noreply@redactado.es>
2026-02-23 15:42:26 +0100

    refactor Encoder: remove EncodeConfig parameter from encode method

--- core/src/main/java/com/redactado/raisu/core/encoding/Encoder.java
+++ core/src/main/java/com/redactado/raisu/core/encoding/Encoder.java
@@ -1,12 +1,10 @@
 package com.redactado.raisu.core.encoding;
 
-import com.redactado.raisu.config.EncodeConfig;
 import com.redactado.raisu.snapshot.Snapshot;
 import java.io.IOException;
 import org.jetbrains.annotations.NotNull;
 
 public interface Encoder {
 
-    @NotNull
-    byte[] encode(@NotNull Snapshot snapshot, @NotNull EncodeConfig config) throws IOException;
+    byte @NotNull [] encode(@NotNull Snapshot snapshot) throws IOException;
 }
