7620c4a713b2801d8d66de8c578ef296dc7e4f53
Reddishye <noreply@redactado.es>
2026-02-23 15:42:18 +0100

    test EncodeConfigBuilderTest: remove encryption and password related tests

--- core/src/test/java/com/redactado/raisu/core/config/EncodeConfigBuilderTest.java
+++ core/src/test/java/com/redactado/raisu/core/config/EncodeConfigBuilderTest.java
@@ -9,20 +9,10 @@ import org.junit.jupiter.api.Test;
 class EncodeConfigBuilderTest {
 
     @Test
-    void testDefaultConfig() {
+    void testDefaultProvider() {
         EncodeConfig config = new EncodeConfigBuilderImpl().build();
 
         assertEquals(PasteProvider.HASTEBIN, config.provider());
-        assertFalse(config.encrypt());
-        assertNull(config.password());
-    }
-
-    @Test
-    void testWithEncryption() {
-        EncodeConfig config = new EncodeConfigBuilderImpl().password("secret").build();
-
-        assertTrue(config.encrypt());
-        assertEquals("secret", config.password());
     }
 
     @Test
@@ -31,10 +21,5 @@ class EncodeConfigBuilderTest {
                 new EncodeConfigBuilderImpl().provider(PasteProvider.PASTES_DEV).build();
 
         assertEquals(PasteProvider.PASTES_DEV, config.provider());
-    }
-
-    @Test
-    void testEncryptWithoutPassword() {
-        assertThrows(IllegalStateException.class, () -> new EncodeConfigBuilderImpl().encrypt(true).build());
     }
 }
